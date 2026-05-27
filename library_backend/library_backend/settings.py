d166b72d9d7b308e62eae308e11f3e75163c8e70
Gbryan Dev <bryangalamgam@gmail.com>
2026-05-28 01:14:17 +0800

    Add STATIC_ROOT for deployment

--- library_backend/library_backend/settings.py
+++ library_backend/library_backend/settings.py
@@ -80,6 +80,7 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 STATIC_URL = 'static/'
+STATIC_ROOT = '/staticfiles'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
